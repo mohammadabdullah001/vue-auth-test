127f68be0824dff207208b74589700ac220fe682
Ashim Kumar Shaha <ashimkumar69@gmail.com>
2022-12-13 20:41:24 +0600

    domain correction

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
     devServer: {
-        host: "app.auth-api.test",
+        host: "auth-api.test",
     },
 });
